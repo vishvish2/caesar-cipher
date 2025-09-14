22f64fc8fd54aeb3847069c6e52271bb80f936de
vishvish2 <vishnubarath909@gmail.com>
2025-09-14 17:27:06 +0100

    format changes

--- caesar cipher.py
+++ caesar cipher.py
@@ -6,7 +6,7 @@ Capitals = ["A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",
             "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z"]
 Cipher_Text = []
 Plain_Text = input("Enter some text to encrypt: ")
-while Plain_Text.isalpha() == False:
+while Plain_Text.isalpha() is False:
     Plain_Text = input("Enter some text to encrypt: ")
 key = int(input("How many places do you want to shift your letters: "))
 for letter in range(0, len(Plain_Text)):
@@ -14,13 +14,13 @@ for letter in range(0, len(Plain_Text)):
         position1 = Alphabet.index(Plain_Text[letter])
         new1 = position1 + key
         if new1 >= 26 or new1 <= -26:
-            new1 = new1%26
+            new1 = new1 % 26
         Cipher_Text.append(Alphabet[new1])
     elif Plain_Text[letter] in Capitals:
         position2 = Capitals.index(Plain_Text[letter])
         new2 = position2 + key
         if new2 >= 26 or new2 <= -26:
-            new2 = new2%26
+            new2 = new2 % 26
         Cipher_Text.append(Capitals[new2])
 
-print("".join(Cipher_Text),"is your encrypted text")
+print("".join(Cipher_Text), "is your encrypted text")
